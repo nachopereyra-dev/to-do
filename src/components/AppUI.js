a2d3e1e268311679214a66ce1398ca2c686f7743
Juan Ignacio Pereyra Noguez <nacho@MacBook-Air-de-Juan.local>
2022-12-17 17:42:11 -0300

    agregando mensaje inicial

--- src/components/AppUI.js
+++ src/components/AppUI.js
@@ -30,7 +30,7 @@ function AppUI() {
 
             { error && <p>Hubo un error</p>}
             { loading && <p>Cargando...</p>}
-            { (!loading && listaDeItembuscados) && <p></p>}
+            { (!loading && listaDeItembuscados) && <p>Crea tu primer tarea</p>}
 
             {listaDeItembuscados.map((tarea,i) => {
               return ( 
